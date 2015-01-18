693a7d16418e391f9bc0adbe62a4a05ae8e63181
Java <JAVAHONK@GMAIL.COM>
2015-01-18 00:41:06 -0500

    New files

---
+++ src/main/java/com/javahonk/action/Struts2PDFDownloadAction.java
@@ -0,0 +1,76 @@
+package com.javahonk.action;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+
+
+public class Struts2PDFDownloadAction{
+
+	@Action(value = "/downloadPDFFileAction")
+	public void downloadCSVFileAction() throws Exception {
+
+		final ServletContext servletContext = ServletActionContext.getServletContext();
+	    final File tempDirectory = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+	    final String temperotyFilePath = tempDirectory.getAbsolutePath();
+ 
+	    String fileName = "JavaHonkStruts2.pdf";
+	    HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("application/pdf");
+	    response.setHeader("Content-disposition", "attachment; filename="+ fileName);
+ 
+	    try {
+ 
+	        CreatePDFStruts2.createPDF(temperotyFilePath+"\\"+fileName);
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        baos = convertPDFToByteArrayOutputStream(temperotyFilePath+"\\"+fileName);
+	        OutputStream os = response.getOutputStream();
+	        baos.writeTo(os);
+	        os.flush();
+	    } catch (Exception e1) {
+	        e1.printStackTrace();
+	    }
+	}
+	
+	private ByteArrayOutputStream convertPDFToByteArrayOutputStream(String fileName) {
+		 
+		InputStream inputStream = null;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+ 
+			inputStream = new FileInputStream(fileName);
+			byte[] buffer = new byte[1024];
+			baos = new ByteArrayOutputStream();
+ 
+			int bytesRead;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				baos.write(buffer, 0, bytesRead);
+			}
+ 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return baos;
+	}
+
+}
